8056ea3fbec4ac0a97987a16f6a9f587231bd9ce
Oleg Adamov <olegadamov84@gmail.com>
2022-10-28 17:47:51 +0300

    some changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,16 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
     </div>
   );
 };
